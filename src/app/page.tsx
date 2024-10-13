f8bf83f46f139912fdaddcbbdab1140b3ce747d6
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-10-13 07:48:40 +0530

    changes
    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ import MainSetterSection from "@/components/customComponents/MainSetterSection/M
 import MainCanvas from "@/components/customComponents/MainCanvas/MainCanvas";
 import HeroBanner from "@/components/customComponents/HeroBanner/HeroBanner";
 import { Button } from "@/components/ui/button";
+import MainSidebar from "@/components/customComponents/MainSidebar/MainSidebar";
 
 
 
@@ -38,19 +39,10 @@ export default function Home() {
 
 
     <div className="mt-24 p-4 min-w-[260px]">
-      <div className="fixed top-20 flex flex-col justify-between gap-4">
+      <div className="fixed top-20">
 
-  
-        <Link href="/test">
-        <Button className="text-xl font-semibold p-6 bg-[#3C99FE]">+ Create A Post</Button>
-        </Link>
+        <MainSidebar/>
 
-        <Link href="/my-details">
-        <p className="text-xl">My Details</p>
-        </Link>
-        <Link href="/my-team">
-        <p className="text-xl">My Team</p>
-        </Link>
 
       </div>
 
---
+++ src/components/customComponents/MainSidebar/MainSidebar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Separator } from "@/components/ui/separator"
+
+
+const MainSidebar = () => {
+  return (
+    <div className="flex flex-col justify-between gap-4">
+
+        <Link href="/test">
+            <Button className="text-xl font-semibold p-6 bg-[#3C99FE]">+ Create A Post</Button>
+        </Link>
+
+        <Link href="/my-details">
+            <p className="text-xl">My Details</p>
+        </Link>
+        <Link href="/my-team">
+            <p className="text-xl">My Team</p>
+        </Link>
+
+        <Separator />
+
+        <Link href="/my-team">
+            <p className="text-xl text-slate-300">About Devs</p>
+        </Link>
+
+
+    </div>
+  )
+}
+
+export default MainSidebar
